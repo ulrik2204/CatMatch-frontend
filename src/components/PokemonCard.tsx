43ab3a4e5f6681577693cc9bc1c9c2bb8526af54
Ulrik Røsby <ulrik@rosby.no>
2023-10-21 18:47:49 +0200

    refactor: move fatching move logic out of Move component

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,19 +1,12 @@
-import { useQuery } from "@tanstack/react-query";
-import { fromApi } from "../lib/fromApi";
 import { type PokemonMove } from "../types/move";
 import { type Pokemon, type Move } from "../types/pokemon";
 import { type ReactElement, useMemo } from "react";
 
 type PokemonCardProps = {
   pokemon: Pokemon;
+  move1: PokemonMove;
+  move2: PokemonMove | undefined;
 };
-function generateTwoUniqueRandomNumbers(max: number): [number, number] {
-  return [2, 7];
-  const rand1 = Math.floor(Math.random() * max);
-  const rand2 = Math.floor(Math.random() * max);
-  if (rand1 === rand2) return generateTwoUniqueRandomNumbers(max);
-  return [rand1, rand2];
-}
 
 const pokemonTypeColors = {
   normal: "bg-[#A8A878]",
@@ -36,18 +29,8 @@ const pokemonTypeColors = {
   fairy: "bg-[#EE99AC]",
 };
 
-export default function PokemonCard({ pokemon }: PokemonCardProps): ReactElement {
-  const selectedMoves = useMemo(() => {
-    const len = pokemon.moves.length;
-    if (len === 1) return pokemon.moves;
-    else if (len === 2) return pokemon.moves;
-    else {
-      const [rand1, rand2] = generateTwoUniqueRandomNumbers(len);
-      return [pokemon.moves[rand1], pokemon.moves[rand2]];
-    }
-  }, [pokemon.moves]);
-
-  const pokemonType = pokemon.types[0].type.name;
+export default function PokemonCard(props: PokemonCardProps): ReactElement {
+  const pokemonType = props.pokemon.types[0].type.name;
 
   return (
     <div className="flex h-80 w-56 flex-col items-center justify-center rounded border bg-pokeborder p-2">
@@ -59,32 +42,32 @@ export default function PokemonCard({ pokemon }: PokemonCardProps): ReactElement
         }`}
       >
         <div className="flex w-11/12 flex-col">
-          <h2 className="font-bold capitalize">{pokemon.name}</h2>
+          <h2 className="font-bold capitalize">{props.pokemon.name}</h2>
           <div className="flex h-32 w-full flex-col items-center justify-center rounded border-4 border-gray-400 bg-white">
             <img
-              src={pokemon.sprites.other["official-artwork"].front_default}
+              src={props.pokemon.sprites.other["official-artwork"].front_default}
               alt="Picture"
               className="h-28 object-contain"
             />
             <div className="flex h-2 w-full items-center justify-center bg-gray-300 text-[0.4rem]">
               {/* Number, height, width */}
-              No: {pokemon.id} | H: {pokemon.height} | W: {pokemon.weight}
+              No: {props.pokemon.id} | H: {props.pokemon.height} | W: {props.pokemon.weight}
             </div>
           </div>
-          {selectedMoves.map((move) => (
-            <div key={move.move.name}>
-              <Move move={move} />
+          <div>
+            <Move move={props.move1} />
+            <div className="h-1"></div>
+          </div>
+          {props.move2 && (
+            <div>
+              <Move move={props.move2} />
               <div className="h-1"></div>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
   );
-}
-
-function getMove(moveUrl: string) {
-  return fromApi<PokemonMove>(moveUrl);
 }
 
 function formatMoveFlavorText(flavorText: string) {
@@ -101,25 +84,24 @@ function formatMoveFlavorText(flavorText: string) {
   return selectedText;
 }
 
-function Move({ move }: { move: Move }) {
+function Move({ move }: { move: PokemonMove }) {
   // TODO: make this input to PokemonCard instead of doing the request here
-  const { data } = useQuery(["move", move.move.url], () => getMove(move.move.url));
 
   const flavorText = useMemo(() => {
-    const moves = data?.flavor_text_entries ?? [];
+    const moves = move?.flavor_text_entries ?? [];
     const selectedText = moves
-      .slice() // To create a copy of the array
+      .slice() // To create a copy of the array because .reverse is in-place
       .reverse()
       .find((entry) => entry.language.name === "en");
     if (!selectedText) return "";
     return formatMoveFlavorText(selectedText.flavor_text);
-  }, [data]);
+  }, [move]);
 
   return (
     <div>
       <div className="flex flex-row justify-between">
-        <h4 className="font-bold capitalize">{move.move.name}</h4>
-        <h4 className="font-semibold">{data?.power}</h4>
+        <h4 className="font-bold capitalize">{move.name}</h4>
+        <h4 className="font-semibold">{move.power}</h4>
       </div>
       <p className="max-h-12 overflow-hidden text-xs leading-tight">{flavorText}</p>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
-import { type ReactElement } from "react";
+import { useMemo, type ReactElement } from "react";
 import PageContainer from "../components/PageContainer";
 import { useQuery } from "@tanstack/react-query";
 import { type Pokemon } from "../types/pokemon";
 import PokemonCard from "../components/PokemonCard";
 import { fromApi } from "../lib/fromApi";
+import { PokemonMove } from "../types/move";
 
 function getRandomPokemonId(prevId = 0) {
   return 512;
@@ -11,6 +12,14 @@ function getRandomPokemonId(prevId = 0) {
   const rand = Math.floor(Math.random() * maxIdNumber);
   if (rand === prevId) return getRandomPokemonId(prevId);
   return rand;
+}
+
+function generateTwoUniqueRandomNumbers(max: number): [number, number] {
+  return [2, 7];
+  const rand1 = Math.floor(Math.random() * max);
+  const rand2 = Math.floor(Math.random() * max);
+  if (rand1 === rand2) return generateTwoUniqueRandomNumbers(max);
+  return [rand1, rand2];
 }
 
 function fetchPokemon() {
@@ -22,7 +31,48 @@ function fetchAllPokemon() {
   return fromApi("https://pokeapi.co/api/v2/pokemon", { query: { limit: 1000 } });
 }
 
+function getMove(moveUrl: string) {
+  return fromApi<PokemonMove>(moveUrl);
+}
+
+function usePokemonAndMoves(id: number) {
+  const { data: pokemon } = useQuery(["single-pokemon"], fetchPokemon);
+
+  const [selectedMove1, selectedMove2] = useMemo(() => {
+    if (!pokemon) return [null, null];
+    const len = pokemon.moves.length;
+    if (len === 1) return pokemon.moves;
+    else if (len === 2) return pokemon.moves;
+    else {
+      const [rand1, rand2] = generateTwoUniqueRandomNumbers(len);
+      return [pokemon.moves[rand1], pokemon.moves[rand2]];
+    }
+  }, [pokemon?.moves]);
+
+  const { data: move1 } = useQuery(["move", selectedMove1?.move.url], () =>
+    selectedMove1 != null ? getMove(selectedMove1.move.url) : undefined,
+  );
+  const { data: move2 } = useQuery(["move", selectedMove2?.move.url], () =>
+    selectedMove2 != null ? getMove(selectedMove2.move.url) : undefined,
+  );
+  if (!pokemon) return null;
+  if (!move1) return null;
+  if (!move2 && pokemon.moves.length > 1) return null;
+
+  return { pokemon, move1, move2 };
+}
+
 export default function IndexPage(): ReactElement {
-  const { data } = useQuery(["single-pokemon"], fetchPokemon);
-  return <PageContainer>{data && <PokemonCard pokemon={data} />}</PageContainer>;
+  const pokemonAndMoves = usePokemonAndMoves(512);
+  return (
+    <PageContainer>
+      {pokemonAndMoves && (
+        <PokemonCard
+          pokemon={pokemonAndMoves.pokemon}
+          move1={pokemonAndMoves.move1}
+          move2={pokemonAndMoves.move2}
+        />
+      )}
+    </PageContainer>
+  );
 }
