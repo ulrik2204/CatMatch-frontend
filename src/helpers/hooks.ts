291a9d36c396f0ae51a01b8974f9a2d7784d4229
Ulrik Røsby <ulrik@rosby.no>
2023-10-23 11:50:55 +0200

    feat: add useLocalStorage hooks

---
+++ src/helpers/hooks.ts
@@ -0,0 +1,53 @@
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
+
+type StorageObject = typeof window.localStorage | typeof window.sessionStorage;
+// useStorage, useLocalStorage and useSessionStorage is taken from, but translated to typescript:
+// https://github.com/WebDevSimplified/useful-custom-react-hooks/blob/main/src/8-useStorage/useStorage.js
+
+/**
+ * Hook to handle operations with localStorage
+ * @param key The key of the object in the storageObject
+ * @param defaultValue The default value of the item in the storageObject
+ * @param storageObject Either localStorage or sessionStorage
+ * @returns An array of [value, setValue, remove]
+ */
+function useStorage<ValueType>(
+  key: string,
+  defaultValue: ValueType,
+  storageObject: StorageObject,
+): [ValueType, Dispatch<SetStateAction<ValueType>>] {
+  const [value, setValue] = useState<ValueType>(() => {
+    const value = storageObject.getItem(key);
+    if (value != null) {
+      return JSON.parse(value);
+    }
+    return defaultValue;
+  });
+
+  useEffect(() => {
+    if (value === undefined) return storageObject.removeItem(key);
+    storageObject.setItem(key, JSON.stringify(value));
+  }, [key, value, storageObject]);
+
+  return [value, setValue];
+}
+
+/**
+ * Handling operations with localStorage
+ * @param key The key of the object in localStorage
+ * @param defaultValue The default value of the item in localStorage.
+ * @returns An array of [value, setValue, remove]
+ */
+export function useLocalStorage<ValueType>(key: string, defaultValue: ValueType) {
+  return useStorage(key, defaultValue, window.localStorage);
+}
+
+/**
+ * Handling operations with sessionStorage
+ * @param key The key of the object in sessionStorage
+ * @param defaultValue The default value of the item in sessionStorage.
+ * @returns An array of [value, setValue, remove]
+ */
+export function useSessionStorage<ValueType>(key: string, defaultValue: ValueType) {
+  return useStorage(key, defaultValue, window.sessionStorage);
+}
