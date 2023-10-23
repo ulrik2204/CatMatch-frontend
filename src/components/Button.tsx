4ff5f41c842084ecad298eb0f0f235dafa93dd2c
Ulrik Røsby <ulrik@rosby.no>
2023-10-23 13:12:21 +0200

    fix: remove some padding from button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -18,7 +18,7 @@ const Button = (props: ButtonProps) => {
   return (
     <button
       onClick={props.onClick}
-      className={`rounded px-4 py-2 shadow-sm ${colorClasses[props.color ?? "primary"]}`}
+      className={`rounded px-3 py-2 shadow-sm ${colorClasses[props.color ?? "primary"]}`}
     >
       {props.children}
     </button>
