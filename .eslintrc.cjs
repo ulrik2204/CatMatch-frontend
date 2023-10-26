74b8f08779ec69d0b750e0742e7b964b144b8e4d
Ulrik Røsby <ulrik@rosby.no>
2023-10-26 18:36:48 +0200

    fix: eslint errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,9 @@
 const config = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
+  parserOptions: {
+    project: "./tsconfig.json",
+  },
   extends: [
     "plugin:@typescript-eslint/recommended-type-checked",
     "plugin:@typescript-eslint/stylistic-type-checked",
@@ -21,6 +24,7 @@ const config = {
     ],
     "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
   },
+  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.cjs"],
 };
 
 module.exports = config;
