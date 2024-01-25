3482b386aaf383d304840ff93562e567f0e8788f
Ulrik Røsby <ulrik@rosby.no>
2024-01-25 01:33:31 +0100

    fix: reduce batch size to 10

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_API_URL = import.meta.env.DEV
   ? "http://localhost:8000"
   : "https://catmatch-server.rosby.no";
-export const CAT_SUGGESTION_BATCH_SIZE = 15;
+export const CAT_SUGGESTION_BATCH_SIZE = 10;
 
 export const STORAGE_KEYS = {
   CAT_JUDGEMENTS: "catJudgements",
